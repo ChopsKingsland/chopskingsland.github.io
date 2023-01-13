6436edbf7c245532ee7511cdebcdd78a03721849
Charlie Kingsland <charliekingsland@outlook.com>
2023-01-13 23:50:30 +0000

    font fix

--- blotterText.js
+++ blotterText.js
@@ -1,11 +1,10 @@
-
-document.fonts.ready.then(function () {
+function createBlotterText() {
     container = document.getElementById("inner");
 
     var text = new Blotter.Text("Charlie Kingsland", {
         family: "'Unbounded', serif",
         fontWeight: 700,
-        size: 100,
+        size: (200 * window.innerWidth) / 2560, // 200px for 2560px wide screen
         fill: "#ffffff",
         paddingLeft: 50,
         paddingRight: 50
@@ -22,4 +21,20 @@ document.fonts.ready.then(function () {
 
     var scope = blotter.forText(text);
     scope.appendTo(container);
-});
+}
+
+function clearElement(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
+
+document.fonts.ready.then(function () {
+    createBlotterText();
+}.bind(this));
+
+window.onresize = function(event) {
+    container = document.getElementById("inner");
+    clearElement(container);
+    createBlotterText();
+};
